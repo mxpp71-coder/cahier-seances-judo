d717754eb4a2831f0e1de37b02517e5676a25949
mxpp71-coder <mxpp71@gmail.com>
2025-09-22 14:36:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ import io
 
 #  CONFIG 
 st.set_page_config(
-    page_title="Cahier de Séances Judo",
-    page_icon="icon-512.png",   # <-- même nom que ton fichier à la racine
-    layout="wide"
-)
-
 st.markdown("""
+    <link rel="manifest" href="manifest.json">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
     <meta name="apple-mobile-web-app-capable" content="yes">
 """, unsafe_allow_html=True)
+
 
 SHEET_NAME = st.secrets["gsheets"]["sheet_name"]
 WORKSHEET  = st.secrets["gsheets"]["worksheet"]
@@ -543,4 +540,5 @@ with tab_consult:
     )
 
 st.caption("Données stockées dans Google Sheets. Partage l’URL de l’app pour y accéder depuis n’importe où (pense au mot de passe).")
+
 
